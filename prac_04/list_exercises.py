35c1894b6dd4524dec53d103cc711f80b3b26bea
elijahang <elijahang77@gmail.com>
2025-10-12 21:12:10 +0800

    add complete code for Basic List Operations

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,23 @@
+"""
+Practical 4
+List Exercises
+"""
+
+"""
+Basic List Operations
+"""
+
+numbers = []
+
+for i in range(1, 6):
+    number = int(input("Number: "))
+    numbers.append(number)
+
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+average = sum(numbers) / len(numbers)
+print(f"The average of the numbers is {average}")
+
+
