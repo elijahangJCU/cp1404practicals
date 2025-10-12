7054735ed1c02fe6211ac49a37ab5d1da19f7869
elijahang <elijahang77@gmail.com>
2025-10-12 20:06:40 +0800

    Refactor total_income program to use functions and improved variable naming

---
+++ prac_04/total_income.py
@@ -0,0 +1,29 @@
+"""
+CP1404/CP5632 Practical
+Starter code for cumulative total income program
+"""
+
+
+def main():
+    """Calculates the monthly cumulative totals for incomes and displays them"""
+    incomes = []
+    num_of_months = int(input("How many months? "))
+
+    for month in range(1, num_of_months + 1):
+        income = float(input(f"Enter income for month {str(month)}: "))
+        incomes.append(income)
+
+    print_report(incomes, num_of_months)
+
+
+def print_report(incomes, num_of_months):
+    """Prints out the monthly cumulative totals for incomes"""
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month in range(1, num_of_months + 1):
+        income = incomes[month - 1]
+        total += income
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+
+
+main()
