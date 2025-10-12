04d20bc80eb9e5669f1c3358256278dfa8ae3e2b
elijahang <elijahang77@gmail.com>
2025-10-12 20:37:22 +0800

    Refactor subject_reader to return nested list and add display function for subject details

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -5,25 +5,13 @@ Data file -> lists program
 
 FILENAME = "subject_data.txt"
 
+subject_info = []
 
-def main():
-    data = load_data(FILENAME)
-    print(data)
+with open(FILENAME, 'r') as subject_data:
+    for i in subject_data:
+        line = i.strip().split(",")
+        line[2] = int(line[2])
+        subject_info.append(line)
 
-
-def load_data(filename=FILENAME):
-    """Read data from file formatted like: subject,lecturer,number of students."""
-    input_file = open(filename)
-    for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
-    input_file.close()
-
-
-main()
+for r in subject_info:
+    print(f"{r[0]} is taught by {r[1]} and has {r[2]} students")
