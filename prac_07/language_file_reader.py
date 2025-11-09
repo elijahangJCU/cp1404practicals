bb1099ce2bc873884054df0fa3eed7d5efe31fb3
elijahang <elijahang77@gmail.com>
2025-11-09 21:21:37 +0800

    Add pointer_arithmetic attribute to ProgrammingLanguage and update language_file_reader to handle new CSV field

--- prac_07/language_file_reader.py
+++ prac_07/language_file_reader.py
@@ -11,11 +11,11 @@ from programming_language import ProgrammingLanguage
 
 
 def main():
-    """Read file of programming language details, save as objects, display."""
+    """Read file of programming language details including pointer arithmetic, save as objects, display."""
     languages = []
     # Open the file for reading
     in_file = open('languages.csv', 'r')
-    # File format is like: Language,Typing,Reflection,Year
+    # File format is like: Language,Typing,Reflection,Year,PointerArithmetic
     # 'Consume' the first line (header) - we don't need its contents
     in_file.readline()
     # All other lines are language data
@@ -26,9 +26,11 @@ def main():
         # print(parts)  # debugging
         # Reflection is stored as a string (Yes/No) and we want a Boolean
         reflection = parts[2] == "Yes"
+        # PointerArithmetic is stored as a string (Yes/No) and we want a Boolean
+        pointer_arithmetic = parts[4] == "Yes"
         # Construct a ProgrammingLanguage object using the elements
         # year should be an int
-        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]))
+        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]), pointer_arithmetic)
         # Add the language we've just constructed to the list
         languages.append(language)
     # Close the file as soon as we've finished reading it
