1d155f660409c34a4d62813ec3f984ae88bd9f43
elijahang <elijahang77@gmail.com>
2025-11-16 19:26:23 +0800

    completed squared exercise

--- prac_08/squaring.py
+++ prac_08/squaring.py
@@ -2,32 +2,33 @@
 CP1404/CP5632 Practical
 Kivy GUI program to square a number
 Estimated time taken to complete: 20 mins
-Actual time taken to complete:
+Actual time taken to complete: 20 mins
 """
 
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
-__author__ = 'Lindsay Ward'
+__author__ = "Lindsay Ward"
 
 
 class SquareNumberApp(App):
-    """ SquareNumberApp is a Kivy App for squaring a number """
+    """ Kivy app for squaring a number """
+
     def build(self):
-        """ build the Kivy app from the kv file """
-        Window.size = (200, 100)
+        """ Build the Kivy app from the kv file """
+        Window.size = (300, 150)
         self.title = "Square Number"
         self.root = Builder.load_file('squaring.kv')
         return self.root
 
     def handle_calculate(self, value):
-        """ handle calculation (could be button press or other call), output result to label widget """
+        """ Handle calculate button press or call """
         try:
             result = float(value) ** 2
             self.root.ids.output_label.text = str(result)
         except ValueError:
-            pass
+            self.root.ids.output_label.text = "0"
 
 
 SquareNumberApp().run()
