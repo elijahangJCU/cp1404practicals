9b20b2c975ca835d39d2745b83cd2ec2edfb38ca
elijahang <elijahang77@gmail.com>
2025-11-24 12:40:01 +0530

    add self.name = name to __init__ and add self.fuel -= distance to drive method

--- prac_09/car.py
+++ prac_09/car.py
@@ -4,13 +4,13 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, name, fuel=0):
         """Initialise a Car instance.
-
         fuel: float, one unit of fuel drives one kilometre
         """
+        self.name = name
         self.fuel = fuel
-        self._odometer = 0
+        self.odometer = 0
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
@@ -24,8 +24,9 @@ class Car:
         """
         if distance > self.fuel:
             distance = self.fuel
-            self.fuel = 0
-        else:
-            self.fuel -= distance
-        self._odometer += distance
+        self.odometer += distance
+        self.fuel -= distance
         return distance
+
+    def __str__(self):
+        return f"{self.name}, fuel={self.fuel}, odo={self.odometer}"
